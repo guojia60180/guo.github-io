4ac240e0a72b0e4c4bfd01f4eb11a26b8daa9739
guojia60180 <44565715+guojia60180@users.noreply.github.com>
2018-11-01 17:06:31 +0800

    Update python实例/tkinter/Radiobutton 选择按钮.py

---
+++ python实例/tkinter/Radiobutton 选择按钮.py
@@ -0,0 +1,30 @@
+#Author guo
+import tkinter as tk
+
+window=tk.Tk()
+window.title('my window')
+window.geometry('500x500')
+
+var=tk.StringVar()
+
+l=tk.Label(window,bg='yellow',width=30,text='empty')
+l.pack()
+def print_selection():
+    l.config(text='you have select'+var.get())
+r1=tk.Radiobutton(window,text='Option A',
+                  variable=var,
+                  value="A",
+                  command=print_selection)
+r1.pack()
+r2=tk.Radiobutton(window,text='Option B',
+                  variable=var,
+                  value="B",
+                  command=print_selection)
+r2.pack()
+r3=tk.Radiobutton(window,text='Option C',
+                  variable=var,
+                  value="C",
+                  command=print_selection)
+r3.pack()
+
+window.mainloop()
