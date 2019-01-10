8f5c95148431c80af0096354412fda3740c572b8
guojia60180 <44565715+guojia60180@users.noreply.github.com>
2019-01-10 22:26:48 +0800

    Create selenium学习7.py

---
+++ 爬虫实例/selenium学习7.py
@@ -0,0 +1,15 @@
+#Author guo
+#前进和后退
+
+import time
+from selenium import webdriver
+
+browser=webdriver.Chrome()
+browser.get('https://www.baidu.com')
+browser.get('https://www.taobao.com')
+browser.get('https://www.python.org')
+
+browser.back()
+time.sleep(1)
+browser.forward()
+browser.close()
