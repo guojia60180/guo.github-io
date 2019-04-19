4c602a0800d39914612b904705df507cc89da43c
guojia60180 <44565715+guojia60180@users.noreply.github.com>
2019-04-19 22:04:31 +0800

    Create test_suite.py

---
+++ unittest使用/test_suite.py
@@ -0,0 +1,14 @@
+#Author guo
+import unittest
+from test_mathfunc2 import TestMathFunc
+
+if __name__=='__main__':
+    suite=unittest.TestSuite()
+
+    tests = [TestMathFunc("test_add"), TestMathFunc("test_minus"), TestMathFunc("test_divide")]
+    suite.addTests(tests)
+
+    runner=unittest.TextTestRunner(verbosity=2)
+
+    runner.run(suite)
+
